603c70f0f16bc8433871954c5c186d8fad984fbf
Praveekorn Chianglong <Praveekorn@gmail.com>
2023-06-18 23:37:14 +0700

    test definition

--- index.js
+++ index.js
@@ -21,51 +21,46 @@ app.post("/webhook", line.middleware(lineConfig), async (req, res) => {
     const events = req.body.events;
     console.log("event=>>>>", events);
     return events.length > 0
-      ? await events.map(item => handleEvent(item)) : res.status(200).send("OK");
+      ? await events.map((item) => handleEvent(item))
+      : res.status(200).send("OK");
   } catch (error) {
     res.status(500).end();
   }
 });
 
 const handleEvent = async (event) => {
-    if(event.type !== 'message' || event.message.type !== 'text'){
-        return null;
-    }
-    else if (event.type === 'message'){
-        const { data } = await axios.get(`https://${env.RAPID_URL}/words/${event.message.text}/synonyms`,{
-            headers: {
-                'x-rapidapi-host':env.RAPID_URL,
-                'x-rapidapi-key': env.RAPID_KEY
-            }
-        })
-        // const { dateDef } = await axios.get(`https://${env.RAPID_URL}/words/${event.message.text}/definitions`,{
-        //     headers: {
-        //         'x-rapidapi-host':env.RAPID_URL,
-        //         'x-rapidapi-key': env.RAPID_KEY
-        //     }
-        // })
-        console.log("Data=>>>>>",data);
-        // console.log("DataDef=>>>>>",dateDef);
-        const { synonyms } = data
-        // const { definitions } = dateDef
-        let str = ''
-        // let str2 = ''
-        synonyms.forEach((result,i)=> {
-            str += synonyms.length -1 !== i ? `${result}\n` : result
-        })
-        // definitions.forEach((result,i)=> {
-        //     str2 += definitions.length -1 !== i ? `${result}\n` : result
-        // })
-        console.log("STR =>>>>>>", str);
-        // console.log("STR2 =>>>>>>", str2);
-        return client.replyMessage(event.replyToken, { type: 'text', text: str });
-    }
-  
+  if (event.type !== "message" || event.message.type !== "text") {
+    return null;
+  } else if (event.type === "message") {
+    const url1 = `https://${env.RAPID_URL}/words/${event.message.text}/synonyms`;
+    const url2 = `https://${env.RAPID_URL}/words/${event.message.text}/definitions`;
+
+    const { data } = await axios.get(
+      `https://${env.RAPID_URL}/words/${event.message.text}/synonyms`,
+      {
+        headers: {
+          "x-rapidapi-host": env.RAPID_URL,
+          "x-rapidapi-key": env.RAPID_KEY,
+        },
+      }
+    );
+    // console.log("Data=>>>>>", data.definitions.map((e)=>e.definition));
+    console.log("Data=>>>>>", data);
+    
+    const { synonyms } = data;
+    let str = "";
+    synonyms.forEach((result, i) => {
+      // str += synonyms.length - 1 !== i ? (i+1)+ "." + " " +`${result}\n` : result;
+      str += (i+1)+ "." + " " +`${result}\n`
+    });
+    console.log("STR =>>>>>>", str);
+    return client.replyMessage(event.replyToken, { type: "text", text: "Word: "+ data.word + "\n" + "Synonyms: " + "\n" + str });
+  }
 };
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(PORT);
+  console.log(PORT);
   console.log(`listening on port ${PORT}`);
 });
